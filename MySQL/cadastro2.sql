ae4c81e5858c1e5c648d444f42c34fbf10a04621
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-07-25 10:10:40 -0300

    novo

---
+++ MySQL/cadastro2.sql
@@ -0,0 +1,8 @@
+use cadastro;
+insert into pessoas
+(id,nome,nascimento,sexo,peso,altura,nacionalidade)
+values
+('1','Godofredo','1983-01-02','M','78.5','1.83','Brasil'),
+('2','Maria','1988-06-21','F','54.5','1.64','Portugal'),
+('3','Renata','1980-11-08','F','50.1','1.68','Uruguai');
+select * from pessoas;
