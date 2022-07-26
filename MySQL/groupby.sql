79e0de03de29f08c87d3ca82d69723d2b2cb242f
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-07-26 12:25:50 -0300

    novo

--- MySQL/groupby.sql
+++ MySQL/groupby.sql
@@ -1,3 +1,3 @@
 use cadastro;
-select carga from cursos
-group by carga;
+select carga from cursos     # Vai selecionar o coluna carga da tabela cursos
+group by carga;              # Vai agrupar todas as cargas de todos os cursos 
