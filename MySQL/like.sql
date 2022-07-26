dbccdc25aadbe70ca02211ea6102dc5a0c345c69
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-07-26 12:30:50 -0300

    novo

--- MySQL/like.sql
+++ MySQL/like.sql
@@ -1,3 +1,4 @@
 use cadastro;
-select * from cursos where nome like '%a'
-#describe cursos;
+select * from cursos
+where nome like '%a';    # Vai selecionar todos os cursos que terminam com a letra a
+describe cursos;
