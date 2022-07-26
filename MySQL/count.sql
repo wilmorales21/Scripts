ebde27ea6b25a39fffb753cd25e61fe553f7c609
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-07-26 12:12:07 -0300

    novo

--- MySQL/count.sql
+++ MySQL/count.sql
@@ -1,3 +1,3 @@
 use cadastro;
-select carga, count(nome) from cursos      # A função de agregação count vai contar quantos cursos tem a mesma carga hor
+select carga, count(nome) from cursos      # A função de agregação count vai contar quantos cursos tem a mesma carga horaria
 group by carga;
