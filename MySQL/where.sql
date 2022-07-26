f5c8d7bd1eb37d8aa94b4418578cb56676ff68b3
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-07-26 12:57:31 -0300

    novo

--- MySQL/where.sql
+++ MySQL/where.sql
@@ -1,6 +1,6 @@
 use cadastro;
 select * from cursos
-where ano <= '2016'
+where ano <= '2016'    # Vai selecionar o nome dos cursos em que o ano for menor ou igual a 2016 
 order by nome;
 describe cursos;
  
