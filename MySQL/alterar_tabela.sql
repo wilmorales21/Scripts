f2fcd8814461454398793c7ac254dbfdfec68abd
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-07-26 11:50:31 -0300

    novo

--- MySQL/alterar_tabela.sql
+++ MySQL/alterar_tabela.sql
@@ -1,4 +1,4 @@
 use cadastro;
 alter table pessoas
-add column profissao varchar(10) after nome;
+add column profissao varchar(10) after nome;     #Vai adicionar a coluna profissão depois da coluna nome
 select * from pessoas;
