abcce7516b121c1ffed46ef82e06d9d8b70abeec
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-07-26 12:08:12 -0300

    novo

--- MySQL/cadastro2.sql
+++ MySQL/cadastro2.sql
@@ -1,6 +1,6 @@
 use cadastro;
 insert into pessoas                                         # Vai inserir informações na tabela pessoas
-(id,nome,nascimento,sexo,peso,altura,nacionalidade)         # Informaç
+(id,nome,nascimento,sexo,peso,altura,nacionalidade)         # Informações que serão inseridas na tabela pessoas
 values
 ('1','Godofredo','1983-01-02','M','78.5','1.83','Brasil'),
 ('2','Maria','1988-06-21','F','54.5','1.64','Portugal'),
