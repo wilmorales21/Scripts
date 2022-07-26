b24df8fe4c30cc124f85a0a87fec5559201fc90b
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-07-26 12:54:29 -0300

    novo

--- MySQL/sum.sql
+++ MySQL/sum.sql
@@ -1,3 +1,3 @@
 use cadastro;
-select sum(totalaulas) from cursos
-where ano = '2016';
+select sum(totalaulas) from cursos     # Vai somar o total de aulas dos cursos no ano de 2014
+where ano = '2014';
