bee3473b6c773ae567b356c6fa2c53093f1b5843
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-07-26 12:51:23 -0300

    novo

--- MySQL/orderby.sql
+++ MySQL/orderby.sql
@@ -1,5 +1,5 @@
 use cadastro;
 select nome,ano,descricao from cursos
-where ano in ('2014','2016','2018')     # Vai ordenar o nome, o ano e a descriç
+where ano in ('2014','2016','2018')     # Vai ordenar o nome, o ano e a descricao dos cursos
 order by ano;
 describe cursos;
