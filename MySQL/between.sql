afd671b6e029c7bc39038483d560ae9d599a2fff
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-07-26 11:54:40 -0300

    novo

--- MySQL/between.sql
+++ MySQL/between.sql
@@ -1,4 +1,4 @@
 use cadastro;
 select nome,ano from cursos
-where ano between 2014 and 2016;
+where ano between 2014 and 2016;   # Vai selecionar as colunas nome e ano da tabela cursos entre os anos de 2014 e 2016.
 describe cursos;
