c99cdb78c6ceb1cfa0c35f5fc57f733a9a7d3915
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-07-26 12:28:10 -0300

    novo

--- MySQL/having.sql
+++ MySQL/having.sql
@@ -1,4 +1,4 @@
 use cadastro;
 select carga, count(nome) from cursos
 group by carga
-having count(nome) > 2;
+having count(nome) > 2;               # Vai mostrar a quantidade de cursos(maior que 2) que tiverem a maior carga horaria
