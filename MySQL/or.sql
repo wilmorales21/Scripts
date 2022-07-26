e3523ce3e22ec5e2927c1eebe945051680c9ecc4
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-07-26 12:45:10 -0300

    novo

--- MySQL/or.sql
+++ MySQL/or.sql
@@ -1,4 +1,4 @@
 use cadastro;
 select nome, totalaulas from cursos
-where carga > 35 or totalaulas < 30;
+where carga > 35 or totalaulas < 30;   # Vai selecionar os cursos que tem a carga maior que 35 ou total de aulas menor que 30
 describe cursos;
