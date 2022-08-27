783f3ec942a27c29f056f8c75d71c41619a661e0
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2022-08-27 15:12:17 -0300

    novo

--- Machine Learning/randomforest.py
+++ Machine Learning/randomforest.py
@@ -3,51 +3,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-arq1 = open('Alegrete.csv','r').read()
-arq2 = open('Variaveis.csv','w')
-
-#converter todo o arq1 em string
-tex = str(arq1)
-
-# Substituindo virgula por ponto
-eb = tex.replace(',','.')
-
-for x in eb:
- arq2.write(x)  
-
-arq2.close()
-#print('Feito?')
-
-#-----------------------------------------------------------------------------------------
-
-df = pd.read_csv('Variaveis.csv',delimiter=' ',nrows=169)
+df = pd.read_csv('variaveis.csv',delimiter=' ',nrows=1009)
 #print(df)
-
-# depois de testar com o codigo teste.py para saber quais sao as variaveis mais importantes, temos:
-# tmax,tmin,urins,urmin,tdins,tdmax
 
 # definir a variavel alvo
 y = df['tins']
 print(y)
 
-# definindo as variaveis tmax tmin urins urmin tdins tdmax para o dataframe x
-# o comando drop vai tirar as colunas indesejaveis
-a = df.drop(columns=['data'])
-b = a.drop(columns=['utc'])
-c = b.drop(columns=['tins'])
-d = c.drop(columns=['urmax'])
-e = d.drop(columns=['tdmin'])
-f = e.drop(columns=['presins'])
-g = f.drop(columns=['presmax'])
-h = g.drop(columns=['presmin'])
-i = h.drop(columns=['vel'])
-j = i.drop(columns=['dir'])
-l = j.drop(columns=['raj'])
-m = l.drop(columns=['rad'])
-x = m.drop(columns=['prec'])
+# essas são as séries selecionadas
+z1 = df['tmax']
+z2 = df['tmin']
+z3 = df['tdins']
+z4 = df['tdmax']
+z5 = df['tdmin']
 
-print('dataframe x:')
-print(x)
+# concatenar as series
+x = pd.concat([z1,z2,z3,z4,z5],axis=1)
+print('serie x:',x)
 
 # a variavel alvo eh y e a variavel de teste eh x  
 
@@ -84,16 +56,21 @@ previsao_final = np.concatenate((A,B),axis=0)
 print('Previsao final:')
 print(previsao_final)
 
+# Converter dados de array para series 
+pf = pd.Series(previsao_final)
+print('Previsao final serie')
+print(pf)
+
 ##############################################################################
 # plota a temperatura observada
-plt.plot(y,color='olivedrab')
+plt.plot(y[770:938],color='olivedrab')
 
 # plota a temperatura prevista pelo random forest
-plt.plot(previsao_final,color='red')
+plt.plot(pf[770:938],color='red')
 
 #plotar legendas
-plt.plot(y,label='Observado',color='olivedrab')
-plt.plot(previsao_final,label='Previsto',color='red')
+plt.plot(y[770:938],label='Observado',color='olivedrab')
+plt.plot(pf[770:938],label='Previsto',color='red')
 plt.legend()
 
 #plota o titulo da figura
@@ -105,9 +82,10 @@ plt.xlabel('Dias')
 plt.grid()
 
 #fixando valores no eixo x
-plt.xticks([24,48,72,96,120,144],['02/01','03/01','04/01','05/01','06/01','07/01'])
+plt.xticks([770,794,818,842,866,890,914,937],['02/02','03/02','04/02','05/02','06/02','07/02','08/02','09/02'])
+
 #fixando os pontos limites entre valor inicial e valor final no eixo x
-plt.xlim([24,167])
+plt.xlim([770,937])
 
 plt.show()
 
