a8ab85e81712102a8633da48499d4ca5000aa883
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2023-02-13 11:29:35 -0300

    del

--- MySQL/cadastro2.sql
+++
@@ -1,8 +0,0 @@
-use cadastro;
-insert into pessoas                                         # Vai inserir informações na tabela pessoas
-(id,nome,nascimento,sexo,peso,altura,nacionalidade)         # Informações que serão inseridas na tabela pessoas
-values
-('1','Godofredo','1983-01-02','M','78.5','1.83','Brasil'),
-('2','Maria','1988-06-21','F','54.5','1.64','Portugal'),
-('3','Renata','1980-11-08','F','50.1','1.68','Uruguai');
-select * from pessoas;
