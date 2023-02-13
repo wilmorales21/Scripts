87b9ecfb33c89a3e9dba033abbbf8c76333acdda
Wiliam Morales <80546143+wilmorales21@users.noreply.github.com>
2023-02-13 12:16:45 -0300

    d

--- MySQL/invocar_arquivo_csv.sql
+++
@@ -1,8 +0,0 @@
-use basededados;
-load data infile '/etc/mysql/mysqlfiles/planilha.csv'
-into table informacoes
-fields terminated by ','
-lines terminated by '\r\n'
-ignore 1 rows
-(ID,produtoID,clienteID,Desconto,Preco,Quantidade,IDarmazenado,ddata);
-select * from informacoes;
